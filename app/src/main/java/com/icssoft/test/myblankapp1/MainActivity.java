0ff7a820198c25d56b82cbe0e387745306728b20
ishan <ishan.chinthaka@gmail.com>
2014-12-14 02:27:47 +0530

    initial commit
    initial commit of blank test app

---
+++ app/src/main/java/com/icssoft/test/myblankapp1/MainActivity.java
@@ -0,0 +1,97 @@
+package com.icssoft.test.myblankapp1;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public static final String EXTRA_MESSAGE = "com.icssoft.test.myfirstapp1.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("My Blank App 1","OnCreate called");
+
+        /*// Check if we're running on Android 5.0 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Call some material design APIs here
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.primary_dark));
+        } else {
+            // Implement this feature without material design
+        }*/
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("My Blank App 1","OnResume called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("My Blank App 1","OnStart called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("My Blank App 1","OnPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("My Blank App 1","OnStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("My Blank App 1","OnDestroy called");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent=new Intent(this,DisplayMessageActivity.class);
+        EditText editText=(EditText)findViewById(R.id.edit_message);
+        String message=editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+}
